8db8f12e060ecf1b2c70e538fddb9d6556fda29e
ClaudiaHdezPerez <claudia.hdez.perez.2004@gmail.com>
2025-05-17 23:28:05 -0400

    fix/updated urls

--- src/chatbot/core.py
+++ src/chatbot/core.py
@@ -1,11 +1,10 @@
 from mistralai.client import MistralClient
 from vector_db.chroma_storage import VectorStorage
-import os
 
 class CubaChatbot:
     def __init__(self):
         self.vector_db = VectorStorage()
-        self.mistral_client = MistralClient(api_key=os.getenv("MISTRAL_API_KEY"))
+        self.mistral_client = MistralClient(api_key="XEV0fCx3MqiG9HqVkGc4Hy5qyD3WwPHr")
         
     def generate_response(self, query):
         context = self.vector_db.similarity_search(query, k=3)
